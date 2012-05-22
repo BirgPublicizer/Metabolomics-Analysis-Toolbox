e4eb4021187f3ff3df8176f56e595ed3aa617a99
BIRG Publicizer <birg.publicizer@gmail.com>
2012-05-22 14:25:12 -0400

    Allowed common scripts to be deleted

--- common_scripts/find_maxs.m
+++
@@ -1,9 +0,0 @@
-function [ maxs ] = find_maxs( coeffs )
-%find_mins - returns an array of index of each maximum
-maxs = [];
-for i=2:length(coeffs)-1
-    if (coeffs(i-1) < coeffs(i) && coeffs(i+1) < coeffs(i))
-        maxs(end+1) = i;
-    end
-end
-
